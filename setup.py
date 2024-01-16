a575140482e50d869077f910a010cdc2bdcb4d14
Sahil Sahu <sahil.sanjay.sahu1@gmail.com>
2024-01-16 18:27:51 +0530

    requirements and setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Metal_Surface_Image_Segmentation"
+AUTHOR_USER_NAME = "SahilSahu73"
+SRC_REPO = "Metal_Surface_Image_Segmentation"
+AUTHOR_EMAIL = "sahil.sanjay.sahu1@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Image segmentation on metal surfaces to detect defects",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
